795de89ccfe938cd6973e0ff7c4d9736dbb680a7
zeynepyucesoy <zeynepyucesoyy@gmail.com>
2015-12-22 12:16:11 +0200

    frek fonksiyonunu içeren dosya eklendi.

--- frek.m
+++ frek.m
@@ -1,15 +1,31 @@
-function ff=frek(nota,oktav) %girilen nota ve oktav degerine gore frekans hesaplayan fonksiyon tanimi
-notalar={'C' 'c' 'D' 'd' 'E' 'F' 'f' 'G' 'g' 'A' 'a' 'B'};%A=La,a(A#,Bb)=#La,B=Si,
-%C=Do,c(C#,Db)=#Do,D=Re,d(D#,Eb)=#Re,E=Mi,F=Fa,f(F#,Gb)=#Fa,G=Sol,g(G#,Ab)=#Sol
-referans=16.35;    %formul icin ilk deger atamasi
-p=length(notalar); %notalar dizisinin eleman sayisi 
-for okt=0:8        %okt(oktav) degerini 0'dan 8(dahil)'e kadar artýrma islemi
-    if okt==oktav  %girilen oktav degerinin okt degeriyle esitlenmesi halinde
-        for j=1:p  %++dizi eleman indisinin artýrýmý
-            if nota==notalar{j} %girilen notanin dizideki indisinin bulunmasi
-                ff=2^okt*(2^((j-1)/12)*referans); %oktav ve nota indisine bagli olarak
-            end                                   %++frekansi hesaplayan formul
+function ff= frek(nota,oktav)
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'};%notalarý bir matrise atýyoruz tüm notalarý ayný boyutta olacak þekilde.
+%c=Do# e=Mi Bemol(Bb) f=Fa# g=Sol# b=Si Bemol
+p=length(notalar);%matristeki notalarýn sayýsý
+referans=16.35;%referans olarak belirlenen frekans
+if nargin<2 %girilen arguman sayýsýnýn 2'den az olma durumu
+  oktav=4;
+     
+    for okt=0:8 %her oktavda notalarý hesaplamak için for döngüsü kulandým.
+        if okt==oktav %eðer döngüden gelen oktav deðeri istenen oktav deðerine eþitse
+            for j=1:p%dizideki notalarýn indexlerini döndüren for döngüsü
+                if size(nota)==size(notalar{j})
+                if nota==notalar{j}%argüman olarak girilen nota adý dizinin j.elemanýyla ayný ise
+                ff=2^okt*(2^((j-1)/12)*referans); %frenkans hesaplanýr
+                if size(nota)==size(notalar{13})%argüman olan nota boyutu eþit ise notalar dizisindeki 13.elemana
+                  if nota==notalar{13}%argüman olan nota notalar dizisindeki 13. elemana eþitse
+                      ff=0; %frekansý 0 a eþitle.
+                  end
+                  end
+                end
+                end  
+                
+            end
+        
         end
     end
+    
 end
-end
+
+
+%f=frek('Dod',2) %fonksiyonu çaðýrýyor
