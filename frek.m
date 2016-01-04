9bfcb4dfc1a25869e8b0e5d5e42e0ae6eda4efd9
aysenurgencdogmus <aysenur812@hotmail.com>
2016-01-04 13:32:33 +0200

    frek.m dosyası düzenlendi.

--- frek.m
+++ frek.m
@@ -1,31 +1,25 @@
-function ff= frek(nota,oktav)
-notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'};%notalarý bir matrise atýyoruz tüm notalarý ayný boyutta olacak þekilde.
-%c=Do# e=Mi Bemol(Bb) f=Fa# g=Sol# b=Si Bemol
-p=length(notalar);%matristeki notalarýn sayýsý
-referans=16.35;%referans olarak belirlenen frekans
-if nargin<2 %girilen arguman sayýsýnýn 2'den az olma durumu
-  oktav=4;
-     
-    for okt=0:8 %her oktavda notalarý hesaplamak için for döngüsü kulandým.
-        if okt==oktav %eðer döngüden gelen oktav deðeri istenen oktav deðerine eþitse
-            for j=1:p%dizideki notalarýn indexlerini döndüren for döngüsü
-                if size(nota)==size(notalar{j})
-                if nota==notalar{j}%argüman olarak girilen nota adý dizinin j.elemanýyla ayný ise
-                ff=2^okt*(2^((j-1)/12)*referans); %frenkans hesaplanýr
-                if size(nota)==size(notalar{13})%argüman olan nota boyutu eþit ise notalar dizisindeki 13.elemana
-                  if nota==notalar{13}%argüman olan nota notalar dizisindeki 13. elemana eþitse
-                      ff=0; %frekansý 0 a eþitle.
-                  end
-                  end
-                end
-                end  
-                
-            end
-        
-        end
-    end
-    
-end
+  function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum 
+  notalar={'do','dod', 're', 'mib' ,'mi' ,'fa', 'fad', 'sol', 'sold', 'la', 'sib', 'si','sus'}; %notalar dizisi oluþturup notalarý ekledim.  
+  referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans aldým.  
+  p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim.  
+  if nargin<2 %argümanlarýn sayýsý 2 den küçükse  
+       oktav=4; %oktavý 4 e eþitle  
+  end  
+ for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.  
+    if i==oktav %i'nin deðeri benim giriþ yaptýðým oktav'ýn deðerine eþitse  
+        for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum  
+             if  size(nota)==size(notalar{j}) %argüman olan nota boyutu eþit ise notalar dizisindeki j.elemana  
+                if nota==notalar{j}%argüman olan nota, notalar dizisindeki j. elemana eþitse  
+                    if j==p%argüman olan nota notalar dizisindeki 13. elemana eþitse  
+                        ff=0; %frekansý 0 a eþitle.  
+                    else 
+                        ff=2^i*(2^((j-1)/12)*referans);%frekansý hesaplýyorum  
+                    end  
+                end   
+            end  
+         end  
+     end  
+  end  
+  end  
 
 
-%f=frek('Dod',2) %fonksiyonu çaðýrýyor
