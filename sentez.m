a0c820247ab7d6273d45ef7415d2b901fc4bfce9
aysenurgencdogmus <aysenur812@hotmail.com>
2016-01-04 13:33:09 +0200

    sentez.m dosyası düzenlendi.

--- sentez.m
+++ sentez.m
@@ -1,25 +1,27 @@
-fs=8192; %örnekleme frekansýný tanýmladým
-duraklama=fs*(1/100); %duraklama tanýmladým
-oktav_degeri=0; %oktav degerinde degisiklik olmama durumu.
-oktavdegis=0; %oktav'a eklenmek istenen deger oktdegis degiskeniyle eklenecek[-1,+1].
-notalar={}; %notalar dizisi oluþturdum
-dosya=fopen('notalar.txt'); %notalar.txt dosyasýný okumak açar.
-[nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %notalar.txt deki deðerleri okur
-fclose(dosya);%dosyayý kapatýr.
-
-if oktav_degeri~=oktavdegis %oktdegis degiskeninde degisiklik yapýlmasý durumunda islenecek sart. 
-    for j=1:length(oktav) %text'ten cekilen oktav degerleri dongu icerisinde degistirilecek.
-        oktav(j)=oktav(j)+oktavdegis; %oktavý arttýrýr
-    end
-end
-
-for i=1:length(nota)
-f=frek(nota{i},oktav(i)); %notalar.txt deki notalarýn frekanslarýný hesaplar
-[topla,t]=note(f,olcu{i}); %frekans ve olcuye göre sinyal oluþturur.
-t=0:1/fs:olcu{i}-(1/fs); %t aralýðý tanýmladým.
-notalar{i}=f; %notalar dizisinin ilk elemanýna frekans
-notalar{i+1}=duraklama; %ikinci elemanýna duraklama süresini atadým.
-plot(t,f,t,duraklama) %frekans ve duraklama süresini çizdirdim.
-plot(t,f,t,echo,t,duraklama) %frekans, echo ve duraklama süresini çizdirdim.
-sound(echo)%ses çalmak için
-end
+Fs=3600; %8192 
+ gecikme=round(Fs/10); 
+ notalar=[]; 
+ duraklama=Fs/10000; %duraklama suresi 
+ oktdegis=3; %oktav'a eklenmek istenen deger oktdegis degiskeniyle eklenecek[-3,+3]. 
+ dosya=fopen('notalar.txt','r'); %text dosyasýný acar,'r':okumak için. 
+ [nota,oktav,olcu]=textread('notalar.txt','%s%d%s','delimiter',','); %bosluk ile ayrýlmýs degiskenler okundu. 
+ fclose(dosya); %text dosyasý kapatýldý. 
+ frekans=zeros(1,length(nota)); 
+ if oktdegis~=0 %oktdegis degiskeninde degisiklik yapýlmasý durumunda islenecek sart.  
+   for j=1:length(oktav) %text'ten cekilen oktav degerleri dongu icerisinde degistirilecek. 
+         oktav(j)=oktav(j)+oktdegis;  
+   end 
+ end 
+ for i=1:length(nota) 
+     frekans(i)=frek(nota{i},oktav(i)); %frek fonsiyonu cagýrýlarak frekans degerleri hesaplandý.  
+     [sindalga,t]=note(frekans(i),str2num(olcu{i})); %note fonksiyonu cagýrýlarakta sin sinyalleri cizildi. 
+     notalar=[notalar sindalga duraklama]; 
+ end   
+ for i=1:length(notalar) 
+     if (i+gecikme)<length(notalar) 
+         notalar(i+gecikme)=notalar(i+gecikme)+0.3*notalar(i);  
+     end 
+ end 
+ notalar = notalar/max(abs(notalar)); 
+ plot(notalar) 
+ sound(notalar,Fs); 
