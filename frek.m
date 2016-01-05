7f55bcf729ac2d86d4f0e981d1fb8bde7f383c1a
zeynepyucesoy <zeynepyucesoyy@gmail.com>
2016-01-05 13:24:26 +0200

    frek.m dosyasi düzenlendi.

--- frek.m
+++ frek.m
@@ -1,10 +1,14 @@
-  function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum 
+%% Fonksiyonlarý tanimladim. 
+function ff=frek(nota,oktav)  %argümanlarý nota ve oktav olan frek fonksiyonu oluþturdum 
+%% Deger tanimlari
   notalar={'do','dod', 're', 'mib' ,'mi' ,'fa', 'fad', 'sol', 'sold', 'la', 'sib', 'si','sus'}; %notalar dizisi oluþturup notalarý ekledim.  
   referans=16.35;  %do notasýnýn 0.oktavdaki deðerini referans aldým.  
   p=length(notalar); %p deðiþkenini notalar dizisinin uzunluðuna eþitledim.  
+ %% Oktav girilmemisse
   if nargin<2 %argümanlarýn sayýsý 2 den küçükse  
        oktav=4; %oktavý 4 e eþitle  
   end  
+  %% Frekans degerini hesaplamak icin
  for i=0:8 %oktav için bir for oluþturdum ve her seferinde bir arttýrdým.  
     if i==oktav %i'nin deðeri benim giriþ yaptýðým oktav'ýn deðerine eþitse  
         for j=1:p %notalar dizindeki elemanlar için for döngüsü oluþturdum  
